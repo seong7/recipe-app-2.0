f04e7e193c0ea76ba0306df177622790c201553b
Seongjin <seongjinkim1123@gmail.com>
2020-05-30 12:16:01 +0900

    test2

--- src/modules/models/models.js
+++ src/modules/models/models.js
@@ -1,4 +1,4 @@
-export * from "./Search/Search.js";
 export * from "./Recipe/Recipe.js";
+export * from "./Search/Search.js";
 export * from "./Likes/Likes.js";
 export * from "./Shopping/Shopping.js";
